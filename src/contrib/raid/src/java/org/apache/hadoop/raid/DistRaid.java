9a5218977613e48a71bd8ce94be85049352c5812
Runhui Li <rhli@proj25>
2014-10-30 17:29:23 +0800

    put same EncodingCandidate to same map

--- src/contrib/raid/src/java/org/apache/hadoop/raid/DistRaid.java
+++ src/contrib/raid/src/java/org/apache/hadoop/raid/DistRaid.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
+import java.util.HashMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
